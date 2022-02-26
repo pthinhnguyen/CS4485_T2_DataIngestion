7d27720d20f77e181b8e77b4bc242fcd7ea6b846
amdiethuong2 <34158577+amdiethuong2@users.noreply.github.com>
2022-02-25 21:09:10 -0600

    Add files via upload

--- import.py
+++ import.py
@@ -15,23 +15,23 @@ from mysql.connector import Error
 #empdata.head()
 import pandas as pd
 from sqlalchemy import create_engine
-db = pd.read_csv('zipcode.csv',index_col=False)
+db = pd.read_csv('updatedDB.csv',index_col=False)
 engine=create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"  
                       .format(user="root", pw="password", 
-                      db="zipcode"))
+                      db="classicmodels"))
 db.head()
-try:
-	conn = msql.connect(host='localhost', user='root', password='password')
-	if conn.is_connected():
-		cursor = conn.cursor()
-		cursor.execute('DROP DATABASE IF EXISTS zipcode;')
-		cursor.execute("CREATE DATABASE zipcode")
-		print("created")
-except Error as e:
-	print("Error ",e)
+#try:
+#	conn = msql.connect(host='localhost', user='root', password='password')
+#	if conn.is_connected():
+#		cursor = conn.cursor()
+#		cursor.execute('DROP DATABASE IF EXISTS class;')
+#		cursor.execute("CREATE DATABASE zipcode")
+#		print("created")
+#except Error as e:
+#	print("Error ",e)
 
 try:
-	conn = msql.connect(host='localhost', database='zipcode', user='root', password='password')
+	conn = msql.connect(auth_plugin='mysql_native_password', host='localhost', database='classicmodels', user='root', password='password')
 	if conn.is_connected():
 		cursor = conn.cursor()
 		cursor.execute('select database();')
